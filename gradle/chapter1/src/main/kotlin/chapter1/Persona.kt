e164e6c77302b81f6de408952ca8ce6ff336e8a6
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-27 20:38:14 -0500

    Se ha editado clase Persona.kt

--- gradle/chapter1/src/main/kotlin/chapter1/Persona.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Persona.kt
@@ -1,11 +1,8 @@
 package chapter1
 
-open class Persona(var nombre:String?=null, var apellido:String?=null
-,var edad:Int?=0, var peso:Double?=0.0, var talla:Double?=0.0)
-{
+open class Persona(var nombre:String?=null, var apellido:String?=null,var edad:Int?=0, var peso:Double?=0.0, var talla:Double?=0.0){
   
-  init
-  {
+  init{
     println("Se ha creado e inicializado objeto 'Persona'")   
   }
   
