0e4df89b4116cde9b29d6e760fa923bce5cfa868
ARIEL <carraro.ariel@gmail.com>
2019-08-27 21:39:47 -0500

    Persona.kt actualizandose

--- gradle/chapter1/src/main/kotlin/chapter1/Persona.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Persona.kt
@@ -1,7 +1,10 @@
 package chapter1
 
-open class Persona(open var nombre:String?=null,open var apellido:String?=null,open var edad:Int?=0,open var peso:Double?=0.0,open var talla:Double?=0.0){
-  
+open class Persona(open var nombre:String?=null,
+                   open var apellido:String?=null,
+                   open var edad:Int?=0,
+                   open var peso:Double?=0.0,
+                   open var talla:Double?=0.0){
   init{
     println("Se ha creado e inicializado objeto 'Persona'")   
   }
