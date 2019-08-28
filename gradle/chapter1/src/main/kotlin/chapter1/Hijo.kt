334fe191957cb5ccafdfbc79219550066e956688
ARIEL <carraro.ariel@gmail.com>
2019-08-27 21:38:56 -0500

    Hijo.kt actualizandose

--- gradle/chapter1/src/main/kotlin/chapter1/Hijo.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Hijo.kt
@@ -1,6 +1,10 @@
 package chapter1
 
-class Hijo(override var nombre:String?=null, override var apellido:String?=null,override var edad:Int?=0.0, override var peso:Double?=0.0, override var talla:Double?=0.0):Persona(nombre,apellido,edad,peso,talla){
+class Hijo(override var nombre:String?=null, 
+           override var apellido:String?=null,
+           override var edad:Int?=0, 
+           override var peso:Double?=0.0, 
+           override var talla:Double?=0.0):Persona(nombre,apellido,edad,peso,talla){
   init{
     println("Se ha creado e inicializado objeto 'Hijo'")
   }
