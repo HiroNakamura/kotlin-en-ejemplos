ed485e7385008e8f09f3b02f2fdab1578dcef604
ARIEL <carraro.ariel@gmail.com>
2019-08-27 20:24:04 -0500

    Main.kt se ha actualizado

--- gradle/chapter1/src/main/kotlin/chapter1/Main.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Main.kt
@@ -1,11 +1,14 @@
 package chapter1
 
-
 fun saludar(nombre:String?=null):String{
    return "Hola, ${nombre}"
 }
 
+fun mockTestA():Unit{
+   val nombre:String = "John Wallager"
+   println(saludar(nombre))
+}
+
 fun main(args: Array<String>) {
-    val nombre:String = "John Wallager"
-    println(saludar(nombre))
+    mockTestA()
 }
