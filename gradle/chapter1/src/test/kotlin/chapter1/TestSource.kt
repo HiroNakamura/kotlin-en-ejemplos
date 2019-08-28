fc71bbd0ef64698f68b8459e7f39d19b94ab3f5b
ARIEL <carraro.ariel@gmail.com>
2019-08-27 20:02:43 -0500

    Se edito test

--- gradle/chapter1/src/test/kotlin/chapter1/TestSource.kt
+++ gradle/chapter1/src/test/kotlin/chapter1/TestSource.kt
@@ -3,6 +3,7 @@ package chapter1
 import kotlin.test.assertEquals
 import org.junit.Test
 
+
 class TestSource{
     @Test fun f() {
         assertEquals("Hola, John Wallager", saludar("Hola, John Wallager"))
