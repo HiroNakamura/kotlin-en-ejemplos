fa166c6d7f111843c86b549b5661e188797079b0
ARIEL <carraro.ariel@gmail.com>
2019-08-27 22:39:17 -0500

    Main.kt actualizado

--- gradle/chapter1/src/main/kotlin/chapter1/Main.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Main.kt
@@ -9,6 +9,15 @@ fun mockTestA():Unit{
    println(saludar(nombre))
 }
 
+fun mockTestB():Unit{
+   val persona:Persona = Persona("Juan Jacobo","Rosseau",45,67.9,1.80)
+   val servicioPersona:IPersonaService = PersonaServiceImpl()
+   println("${persona}")
+   println("${servicioPersona.isMayor(persona)? 'Es mayor de edad' : 'Es menor de edad'}")
+   println("${servicioPersona.getImc(persona)}")
+}
+
 fun main(args: Array<String>) {
-    mockTestA()
+    //mockTestA()
+    mockTestB()
 }
