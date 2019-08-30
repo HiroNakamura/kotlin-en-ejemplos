3e4c2aade8d26248846112ddde451d6a8f8a42fb
ARIEL <carraro.ariel@gmail.com>
2019-08-30 12:05:14 -0500

    Estructuras.kt editado

--- gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
@@ -10,8 +10,26 @@ fun recorrer_array():Unit{
   println("Tam: ${arregloString.size}}")
   println("Valor del indice 1: "+arregloString[1])
   println("Valor del indice 0: "+arregloString.get(0))
+  
+  //Recorrer sin índice
   arregloString.forEach(it->{
     println(it)
   });
+  
+  println()
+  
+  //recorrer con índice 
+  arregloString.forEach(indice, arregloValor->{
+    println("${indice} : ${arregloValor}")
+  });
+  
+  println()
+  
+  val arregloInteger:Array<Int>=arrayOf<Int>(0,1,2,3,4,5,6,7,8,9)
+  for(entero in arregloInteger){
+    println("Hola no. ${entero}")
+  }
+  
+  
   
 }
