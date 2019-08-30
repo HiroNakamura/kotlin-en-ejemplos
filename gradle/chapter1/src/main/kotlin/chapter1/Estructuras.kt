679669ccf8135a877497bd79e62e889aff3cfa5a
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-30 13:39:16 -0500

    Seha editado Estructuras.kt

--- gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
@@ -8,7 +8,7 @@ package chapter1
 
 fun recorrer_mapa():Unit{
   val mapaEnteros = mapOf(0 to "0",1 to "1",2 to "2",3 to "3",4 to "4",5 to "5",6 to "6",7 to "7",8 to "8",9 to "9")
-  if(mapaEnteros.size()>0){
+  if(mapaEnteros.size > 0){
     println("Mapa lleno")
     println("Tam: ${mapaEnteros.count()}")
   }
