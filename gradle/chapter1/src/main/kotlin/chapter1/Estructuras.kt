bf7bef7ff4fad23cffa2554c2742bd403061af66
Fer_Carraro <carraro.fernando@gmail.com>
2019-08-30 16:25:53 -0500

    Se ha editado Estructuras

--- gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
@@ -21,7 +21,6 @@ fun recorrer_lista_mapa_mutables():Unit{
 }
 
 
-
 fun recorrer_mapa():Unit{
   val mapaEnteros = mapOf(0 to "0",1 to "1",2 to "2",3 to "3",4 to "4",5 to "5",6 to "6",7 to "7",8 to "8",9 to "9")
   if(mapaEnteros.size > 0){
