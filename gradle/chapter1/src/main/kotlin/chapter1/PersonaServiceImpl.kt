763db6bfa32bd5260d0a319a5ece6359b520a124
ARIEL <carraro.ariel@gmail.com>
2019-08-30 11:06:40 -0500

    Se edito PersonaServiceImpl
    Se colocó autor

--- gradle/chapter1/src/main/kotlin/chapter1/PersonaServiceImpl.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/PersonaServiceImpl.kt
@@ -1,5 +1,10 @@
 package chapter1
 
+/**
+*@author Fer Carraro
+*@date 30/agosto/2019
+*/
+
 class PersonaServiceImpl:IPersonaService{
    
    override fun isMayor(persona:Persona):Boolean=persona.edad>18
