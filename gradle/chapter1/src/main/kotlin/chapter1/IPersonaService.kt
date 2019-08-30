996897a674969c5abb9373140faf024e5fdf1fc3
ARIEL <carraro.ariel@gmail.com>
2019-08-30 11:05:29 -0500

    Interface editada

--- gradle/chapter1/src/main/kotlin/chapter1/IPersonaService.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/IPersonaService.kt
@@ -1,5 +1,10 @@
 package chapter1
 
+/**
+*@author Fer Carraro
+*@date 30/agosto/2019
+*/
+
 interface IPersonaService{
    fun isMayor(persona:Persona):Boolean
    fun getImc(persona:Persona):Double
