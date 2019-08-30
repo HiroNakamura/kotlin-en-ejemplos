eaaea55fc05fbdeca990f8beced05f0d832a4004
ARIEL <carraro.ariel@gmail.com>
2019-08-30 11:42:03 -0500

    Estructuras.kt editado

--- gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Estructuras.kt
@@ -4,3 +4,14 @@ package chapter1
 *@author Fer Carraro
 *@date 30/agosto/2019
 */
+
+fun recorrer_array():Unit{
+  val arregloString = arrayOf("Garita Perez","Kotlin for Beginners","Odiseo salva la granja")
+  println("Tam: ${arregloString.size}}")
+  println("Valor del indice 1: "+arregloString[1])
+  println("Valor del indice 0: "+arregloString.get(0))
+  arregloString.forEach(it->{
+    println(it)
+  });
+  
+}
