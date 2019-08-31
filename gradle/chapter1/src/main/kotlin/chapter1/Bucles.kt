1d60539ec6a0e650e80108886b0b452be3acfae1
ARIEL <carraro.ariel@gmail.com>
2019-08-31 10:16:43 -0500

    Bucles.kt editado

--- gradle/chapter1/src/main/kotlin/chapter1/Bucles.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Bucles.kt
@@ -17,6 +17,16 @@ fun ciclo_while():Unit{
        println("Hola no. $cont")
      }
   }
+  
+  println()
+  cont = 0
+  while(cont < TAM){
+    if(cont%4==0){
+      println("$cont")
+    }
+    cont++;
+  }
+  
 }
 
 
@@ -24,17 +34,39 @@ fun ciclo_for(){
    var conteo:Int=0;
    val miLista = listOf("Aldebran","Casius","Andromeda","Apolo")
    println("#Ciclo for:")
+   //for range
    for(i in 0..TAM){
      if(i%3==0 && i%5==0){
        println("$conteo: $i")
      }
      conteo++;
    }
-  
   println()
+  //for collection
   for(item in miLista){
     println("item: $item")
   }
+  println()
+  //for until
+  for(i in 20 until TAM){
+    if(i%5==0 && i%3==0){
+      println("Ciao no. $i")
+    }
+  }
+  println()
+  //for downTo
+  for(i in 40 downTo 0){
+    if(i%5==0 && i%3==0){
+      println("Ciao no. $i")
+    }
+  }
+  println()
+  //for downTo, step
+  for(i in TAM downTo 0 step 10){
+    if(i%5==0 && i%3==0){
+      println("Ciao no. $i")
+    }
+  }
+  
+  
 }
-
-
