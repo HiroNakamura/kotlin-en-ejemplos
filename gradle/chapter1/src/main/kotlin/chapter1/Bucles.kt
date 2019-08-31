533dc385e89981b9c0f3113d39b97e32d419d90c
ARIEL <carraro.ariel@gmail.com>
2019-08-30 19:09:16 -0500

    Bucles.kt
    Se ha editado

--- gradle/chapter1/src/main/kotlin/chapter1/Bucles.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Bucles.kt
@@ -13,7 +13,7 @@ fun ciclo_while():Unit{
   var cont:Int = 0
   println("#Ciclo while:")
   while(cont++ < TAM){
-     if(cont%3 == 0 and cont%5==0){
+     if(cont%3 == 0 && cont%5==0){
        println("Hola no. $cont")
      }
   }
