1dc40760101e5a4140563a01ac5c2271c1f2183b
ARIEL <carraro.ariel@gmail.com>
2019-09-04 16:04:29 -0500

    Excepciones.kt ha sido creado

---
+++ gradle/chapter1/src/main/kotlin/chapter1/Excepciones.kt
@@ -0,0 +1,20 @@
+
+package chapter1
+
+/**
+*@author Fer Carraro
+*@date 03/septiembre/2019
+*/
+
+fun excepcionTestA():Unit{
+    try {
+      println()
+    }
+    catch (e: Exception) {
+        println("Alguna Exception ha ocurrido")
+    }
+    finally {
+    
+    }
+
+}
