e7119f0a5b9ac3a5beda67aed69abc463bd573eb
ARIEL <carraro.ariel@gmail.com>
2019-09-04 16:30:30 -0500

    Excepciones.kt

--- gradle/chapter1/src/main/kotlin/chapter1/Excepciones.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Excepciones.kt
@@ -7,13 +7,32 @@ package chapter1
 */
 
 fun excepcionTestA():Unit{
+    println("Excepciones:")
     try {
       println(45/0)
     }
     catch (e: Exception) {
         println("Alguna Exception ha ocurrido: ${e.toString()}")
+        e.printStackTrace();
     }
     finally {
     	println("Esto siempre se ejecutara")
+    }
+}
+
+fun excepcionTestB():Unit{
+    println("Excepciones:")
+    try{
+       println(45/0)
+    }
+    catch(ae:ArithmeticException){
+      println("Ha ocurrido una ArithmeticException: ${ae.toString()}")
+    }
+    catch(re:RuntimeException){
+      println("Alguna RuntimeException ha ocurrido: ${re.toString()}")
+    }
+    catch(ex:Exception){
+       println("Alguna Exception ha ocurrido: ${e.toString()}")
+       e.printStackTrace();
     }
 }
