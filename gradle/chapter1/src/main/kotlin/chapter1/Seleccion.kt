79e9d4a9eb11acac7223fbe84e0cd7205a9e8731
ARIEL <carraro.ariel@gmail.com>
2019-09-06 21:43:06 -0500

    Se edito Seleccion.kt

--- gradle/chapter1/src/main/kotlin/chapter1/Seleccion.kt
+++ gradle/chapter1/src/main/kotlin/chapter1/Seleccion.kt
@@ -18,11 +18,27 @@ fun seleccion_if(edad:Int):Unit{
 
 fun seleccion_when(opc:Int):Unit{
     val resultado = when(opc){
-       1 -> "Has elegido comer la pastilla de la fantasía"
-       2 -> "Has elegido el libro de los secretor de Don Gaspar"
+       1 -> "Has elegido comer la pastilla de la fantasía."
+       2 -> "Has elegido el libro de los secretos del Péndulo de Foucault."
+       3 -> "Las interminencias del tablero de las llamas." 
+       in 4..10 -> "Las fabulosas galletas de la abuela."
        else ->{
-          "En un horizonte se ha presentado el señor conejo"
+          "En un horizonte se ha presentado el señor conejo."
        }
     }
     println("$resultado")
+}
+
+fun seleccion_when_any(dato:Any):Unit{
+    val resultado = when(dato){
+       is Byte -> "El dato $dato es Byte."
+       is Short -> "El dato $dato es Short."
+       is Char -> "El dato $dato es Char."
+       is Int -> "El dato $dato es Int."
+       is Long -> "El dato $dato es Long."
+       is Float -> "El dato $dato es Float."
+       is Double -> "El dato $dato es Double."
+       is Boolean -> "El dato $dato es Boolean."
+       is String -> "El dato $dato es String."
+    }
 }
