7e89d53c282ded9b421e1dd10af7707d8a2ad53a
ARIEL <carraro.ariel@gmail.com>
2019-09-07 18:40:54 -0500

    Se ha editado Funciones.kt

--- gradle/chapter2/src/main/kotlin/chapter2/Funciones.kt
+++ gradle/chapter2/src/main/kotlin/chapter2/Funciones.kt
@@ -6,3 +6,10 @@ package chapter2
 *@date 07/09/2019
 */
 
+import spark.Spark.*
+
+
+//http://localhost:4567/holaKotlin
+fun getHola(){
+   get("/holaKotlin") { req, res -> "Bienvenidos a Kotlin en ejemplos." }
+}
