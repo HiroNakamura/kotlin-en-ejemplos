029e111a844f05612d3892cf04dc8c8a31e74694
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-10 13:54:19 -0500

    Se edito Funciones y Departamento .kt

--- gradle/chapter2/src/main/kotlin/chapter2/DepartamentoDao.kt
+++ gradle/chapter2/src/main/kotlin/chapter2/DepartamentoDao.kt
@@ -5,9 +5,17 @@ package chapter2
 *@date 09/09/2019
 */
 
+
+import java.util.concurrent.atomic.AtomicLong
+
 class DepartamentoDao{
+
     val departamentos = hashMapOf(0 to Departamento(1L, "Direccion General", "Thomas Muller")
     ,1 to Departamento(2L, "Recursos Humanos", "Irene Villa")
     ,2 to Departamento(3L, "Informatica", "Carlos Castelar")
     )
+
+    var ultimoId: AtomicLong = AtomicLong(departamentos.size.toLong() - 1)
+    
+
 }
--- gradle/chapter2/src/main/kotlin/chapter2/Funciones.kt
+++ gradle/chapter2/src/main/kotlin/chapter2/Funciones.kt
@@ -7,26 +7,26 @@ package chapter2
 */
 
 import spark.Spark.*
-import com.fasterxml.jackson.core.*;
-import com.fasterxml.jackson.core.io.CharacterEscapes;
-import com.fasterxml.jackson.core.io.SegmentedStringWriter;
-//import com.fasterxml.jackson.core.json.JsonFactory;
-import com.fasterxml.jackson.core.type.ResolvedType;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.core.util.*;
+//import com.fasterxml.jackson.core.*;
+//import com.fasterxml.jackson.core.io.CharacterEscapes;
+//import com.fasterxml.jackson.core.io.SegmentedStringWriter;
+////import com.fasterxml.jackson.core.json.JsonFactory;
+//import com.fasterxml.jackson.core.type.ResolvedType;
+//import com.fasterxml.jackson.core.type.TypeReference;
+//import com.fasterxml.jackson.core.util.*;
 
-import com.fasterxml.jackson.databind.cfg.*;
-import com.fasterxml.jackson.databind.deser.*;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
-import com.fasterxml.jackson.databind.introspect.*;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitorWrapper;
-import com.fasterxml.jackson.databind.jsontype.SubtypeResolver;
-import com.fasterxml.jackson.databind.node.*;
-import com.fasterxml.jackson.databind.ser.*;
-import com.fasterxml.jackson.databind.type.*;
-import com.fasterxml.jackson.databind.util.ClassUtil;
-import com.fasterxml.jackson.databind.util.RootNameLookup;
-import com.fasterxml.jackson.databind.util.TokenBuffer;
+//import com.fasterxml.jackson.databind.cfg.*;
+//import com.fasterxml.jackson.databind.deser.*;
+//import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+//import com.fasterxml.jackson.databind.introspect.*;
+//import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitorWrapper;
+//import com.fasterxml.jackson.databind.jsontype.SubtypeResolver;
+//import com.fasterxml.jackson.databind.node.*;
+//import com.fasterxml.jackson.databind.ser.*;
+//import com.fasterxml.jackson.databind.type.*;
+//import com.fasterxml.jackson.databind.util.ClassUtil;
+//import com.fasterxml.jackson.databind.util.RootNameLookup;
+//import com.fasterxml.jackson.databind.util.TokenBuffer;
 import com.fasterxml.jackson.databind.ObjectMapper
 
 
