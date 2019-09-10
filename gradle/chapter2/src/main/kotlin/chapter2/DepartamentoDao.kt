968a3c356224b91673be22876e8ed844da124ce3
Fer_Carraro <carraro.fernando@gmail.com>
2019-09-10 16:48:24 -0500

    Se ha editado DepartamentoDao .kt

--- gradle/chapter2/src/main/kotlin/chapter2/DepartamentoDao.kt
+++ gradle/chapter2/src/main/kotlin/chapter2/DepartamentoDao.kt
@@ -10,12 +10,32 @@ import java.util.concurrent.atomic.AtomicLong
 
 class DepartamentoDao{
 
-    val departamentos = hashMapOf(0 to Departamento(1L, "Direccion General", "Thomas Muller")
-    ,1 to Departamento(2L, "Recursos Humanos", "Irene Villa")
-    ,2 to Departamento(3L, "Informatica", "Carlos Castelar")
+    val departamentos = hashMapOf(0L to Departamento(1L, "Direccion General", "Thomas Muller")
+    ,1L to Departamento(2L, "Recursos Humanos", "Irene Villa")
+    ,2L to Departamento(3L, "Informatica", "Carlos Castelar")
     )
 
     var ultimoId: AtomicLong = AtomicLong(departamentos.size.toLong() - 1)
     
+    fun save(nombre: String, responsable: String) {
+        val id = ultimoId.incrementAndGet()
+        departamentos.put(id, Departamento(id = id,nombre = nombre, responsable = responsable))
+    }
+
+    fun findById(id: Long): Departamento? {
+        return departamentos[id]
+    }
+
+    fun findByNombre(nombre: String): Departamento? {
+        return departamentos.values.find { it.nombre == nombre }
+    }
+
+    fun update(id: Long, nombre: String, responsable: String) {
+        departamentos.put(id, Departamento(id = id, nombre = nombre, responsable = responsable))
+    }
+
+    fun delete(id: Long) {
+        departamentos.remove(id)
+    }
 
 }
