a4622d0cd91548e0127e9e9cdc901d409cc25ec9
ARIEL <carraro.ariel@gmail.com>
2019-09-10 18:29:32 -0500

    Se ha editado cliente.js
    Se agregó método fetch para consumir servicio REST

--- gradle/chapter2/app/js/cliente.js
+++ gradle/chapter2/app/js/cliente.js
@@ -1,1 +1,20 @@
 /*cliente REST*/
+
+var cliente={
+   init: function(){
+     console.log('Cliente REST');
+     
+     fetch('http://localhost:4567/api/departamentos').then(response => response.json())
+        .then(departamentos =>{
+            for(var departamento in departamentos){
+                console.log('Departamento: ',departamentos[departamento].nombre);
+            }
+        })
+        .catch(e=>{
+            console.error("Ha ocurrido un error en el servidor");
+        })
+     
+   }
+};
+
+window.addEventListener("load",cliente.init);
