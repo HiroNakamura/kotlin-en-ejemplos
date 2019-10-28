e06352b2d5a876ea82a6645c66eaeb8bf6d9513a
Fer_Carraro <carraro.fernando@gmail.com>
2019-10-27 18:17:47 -0600

    Se ha agregado chapter4

---
+++ maven/chapter4/src/main/kotlin/com/inforhomex/batch/Chapter4Application.kt
@@ -0,0 +1,11 @@
+package com.inforhomex.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Chapter4Application
+
+fun main(args: Array<String>) {
+	runApplication<Chapter4Application>(*args)
+}
---
+++ maven/chapter4/src/test/kotlin/com/inforhomex/batch/Chapter4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.inforhomex.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter4ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
